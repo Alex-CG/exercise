c128990ee8fa98f0d80b5e356d9dee4d089ac209
Alex Contreras <alexcontreras.alxlabs@gmail.com>
2024-11-22 16:44:38 -0500

    first commit

---
+++ src/main/java/test/code/exercises/Exercise01.java
@@ -0,0 +1,10 @@
+package test.code.exercises;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hi");
+
+    }
+}
